aa4ecfbba4b7589f2272eec3e7cd6416f8433c3b
Ricardo Almeida <rdlalmeida@gmail.com>
2022-09-12 22:34:15 +0100

    Adding Chapter 4.0 stuff

--- emerald-dapp/pages/index.js
+++ emerald-dapp/pages/index.js
@@ -1,38 +1,64 @@
 import Head from 'next/head'
 import styles from "../styles/Home.module.css"
 import Nav from '../components/Nav.jsx'
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
+import * as fcl from "@onflow/fcl"
 
 export default function Home() {
   const [newGreeting, setNewGreeting] = useState('');
 
+  const [greeting, setGreeting] = useState('');
+
   function runTransaction() {
+    console.log("Running transaction!");
     console.log("Hi there! The current Greeting is ".concat(newGreeting))
   }
+
+  async function executeScript() {
+    const response = await fcl.query({
+      cadence: `
+      import HelloWorld from 0xb7fb1e0ae6485cf6
+
+      pub fun main(): String {
+        return HelloWorld.greeting
+      }
+      `,
+      args: (arg, t) => []
+    })
+
+    // console.log("Response from out script: " + response)
+    setGreeting(response);
+  }
+
+  useEffect(() => {
+    executeScript()
+  }, [])
 
   function printGoodbye() {
     console.log("Goodbye cruel, horrible world!")
   }
 
-  return(
-    <div className={styles.container}>
+  return (
+    <div>
       <Head>
         <title>Emerald DApp</title>
-        <meta name="description" content="Created by Emerald Academy"/>
-        <link rel="icon" href="https://i.imgur.com/hvNtbgD.png"/>
+        <meta name="description" content="Created by Ricardo, the Emerald Academy instructor!"/>
+        <link rel="icon" href="https://i.imgur.com/hvNtbgD.png" />
       </Head>
 
-      <Nav/>
+      <Nav />
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to my <a href="https://www.youtube.com/watch?v=dQw4w9WgXcQ" target="_blank" >Emerald DApp!</a>
+          Welcome to my <a href="https://academy.ecdao.org" target="_blank">Emerald DApp!</a>
         </h1>
-        <p>What the hell is this &lt;p&gt; tag going to do?</p>
-        <div>
-          <button onClick={runTransaction} className={styles.flex}>Run Transaction</button>
-          <input onChange={(e) => setNewGreeting(e.target.value)} placeholder="Hello, idiots!" />
+        <p>This is a DApp created by Ricardo, the Unlucky</p>
+
+        <div className={styles.flex}>
+          <button onClick={runTransaction}>Run Transaction</button>
+          <input onChange={(e) => setNewGreeting(e.target.value)} placeholder="Hello, Idiots!" />
         </div>
+        <p> {greeting} </p>
       </main>
     </div>
   )
