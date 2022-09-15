bbf2b8f441bf0afa6d68a665d234720182fccfcc
Ricardo Almeida <rdlalmeida@gmail.com>
2022-09-15 15:29:24 +0100

    Adding Chapter 4.0 stuff

--- emerald-dapp/pages/index.js
+++ emerald-dapp/pages/index.js
@@ -9,10 +9,90 @@ export default function Home() {
 
   const [greeting, setGreeting] = useState('');
 
-  function runTransaction() {
-    console.log("Running transaction!");
-    console.log("Hi there! The current Greeting is ".concat(newGreeting))
+  async function runTransaction() {
+    const transactionId = await fcl.mutate({
+      cadence: `
+      import HelloWorld from 0xb7fb1e0ae6485cf6
+
+      transaction(myNewGreeting: String) {
+        prepare(signer: AuthAccount){
+
+        }
+
+        execute {
+          HelloWorld.changeGreeting(newGreeting: myNewGreeting)
+        }
+      }
+      `,
+      args: (arg, t) => [
+        arg(newGreeting, t.String)
+      ],
+      proposer: fcl.authz,
+      payer: fcl.authz,
+      authorizations: [fcl.authz],
+      limit: 999
+    })
+
+    console.log("Here is the transactionId: " + transactionId);
+    await fcl.tx(transactionId).onceSealed();
+    executeScript();
   }
+
+  // Set the variable to use for the update to updated on any change in the input field
+const [newNumber, setNewNumber] = useState('')
+
+async function updateNumber() {
+  const transactionId = await fcl.mutate({
+    cadence: `
+    import SimpleTest from 0x6c0d53c676256e8c
+
+    transaction(myNewNumber: Int) {
+      prepare (signer: AuthAccount) {
+
+      }
+
+      execute {
+        // Run the updateNumber routing with the value currently set in newNumber (passed as argument bellow)
+        SimpleTest.updateNumber(newNumber: myNewNumber)
+      }
+    }
+    `,
+    args: (arg, t) => [
+      arg(newNumber, t.Int)
+    ],
+    proposer: fcl.authz,
+    payer: fcl.authz,
+    authorizations: [fcl.authz],
+    limit: 999
+  })
+
+  // Inform about the tx id attributed to this operation
+  console.log("Transaction submitted with ID: " + transactionId)
+
+  // Wait for the confirmation from the transaction
+  await fcl.tx(transactionId).onceSealed();
+
+  // Run the script to read the new number in the contract
+  readSimpleTest()
+}
+
+async function readSimpleTest() {
+  const response = await fcl.query({
+    cadence: `
+    import SimpleTest from 0x6c0d53c676256e8c
+
+    // The script returns an Int but I'm setting the script to return a String on purpose
+    pub fun main(): String {
+      return "Got this from the SimpleTest contract: ".concat(SimpleTest.number.toString())
+    }
+    `,
+    args: (arg, t) => []
+  })
+
+  // Output the script results the console:
+  console.log(response)
+}
+
 
   async function executeScript() {
     const response = await fcl.query({
@@ -26,17 +106,92 @@ export default function Home() {
       args: (arg, t) => []
     })
 
-    // console.log("Response from out script: " + response)
-    setGreeting(response);
+    console.log("Response from our script: " + response)
+    // setGreeting(response);
+  }
+
+  async function testTypes() {
+
+    // Set the inputs to test
+    const a = '25'
+    const b = 'Does this work at all?'
+    const c = '14.33'
+    const d = '0xb7fb1e0ae6485cf6'
+    const e = false
+    const f = null
+    const g = ['5', '4', '9']
+    const h = [
+      {key: 'Blocto', value: '0x82dd07b1bcafd968'},
+      {key: 'Dapper', value: '0x37f3f5b3e0eaf6ca'}
+    ]
+
+    const inputArray = [a, b, c, d, e, f, g, h]
+
+    const randomIndex = Math.floor(Math.random() * inputArray.length).toString();
+
+    const response = await fcl.query({
+      cadence: `
+      pub fun main(
+        a: Int,
+        b: String,
+        c: UFix64,
+        d: Address,
+        e: Bool,
+        f: String?,
+        g: [Int],
+        h: {String: Address},
+        index: Int
+      ): AnyStruct {
+        // Start by assembling every input into a single array
+        let input_array: [AnyStruct] = [a, b, c, d, e, f, g, h]
+
+        // Check the validity of the index provided:
+        if (index < 0 || index >= input_array.length) {
+          panic(
+            "Invalid index provided: "
+            .concat(index.toString())
+            .concat(". Please provide an integer between 0 and ")
+            .concat((input_array.length - 1).toString())
+            .concat(" to continue")
+            )
+        }
+
+        // Return the element whose index was provided as the last argument
+        return input_array[index]
+      }
+      `,
+      args: (arg, t) => [
+        arg(a, t.Int),
+        arg(b, t.String),
+        arg(c, t.UFix64),
+        arg(d, t.Address),
+        arg(e, t.Bool),
+        arg(f, t.Optional(t.String)),
+        arg(g, t.Array(t.Int)),
+        arg(h, t.Dictionary({key: t.String, value: t.Address})),
+        arg(randomIndex, t.Int)
+      ]
+    })
+
+    // Print out whatever was returned as a response
+    console.log("The " + randomIndex.toString() + "-th element of the type array is " + response)
   }
 
   useEffect(() => {
-    executeScript()
+    // executeScript()
+    // readSimpleTest()
+    testTypes()
   }, [])
 
   function printGoodbye() {
     console.log("Goodbye cruel, horrible world!")
   }
+
+  /*
+    <div className={styles.flex}>
+      <button onClick={readSimpleTest}>Read Simple Test</button>
+    </div>
+  */
 
   return (
     <div>
@@ -56,9 +211,15 @@ export default function Home() {
 
         <div className={styles.flex}>
           <button onClick={runTransaction}>Run Transaction</button>
-          <input onChange={(e) => setNewGreeting(e.target.value)} placeholder="Hello, Idiots!" />
+          <input onChange={(e) => setNewGreeting(e.target.value)} placeholder="Hello, Idiots!" /> 
         </div>
-        <p> {greeting} </p>
+        <div>
+        <p> Current greeting is: {newGreeting} </p>
+        </div>
+        <div className={styles.flex}>
+          <button onClick={updateNumber}>UpdateNumber</button>
+          <input onChange={(f) => setNewNumber(f.target.value)} placeholder="New Number?" />
+        </div>
       </main>
     </div>
   )
